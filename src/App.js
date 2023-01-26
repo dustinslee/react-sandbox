187942c5da64a9e0de3855345fcc1d9cbd5de6f4
Dustin Lee <dustinsl094@gmail.com>
2023-01-26 10:40:57 -0500

    React intro exercise solution

--- src/App.js
+++ src/App.js
@@ -6,10 +6,33 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>React Intro Exercise</h1>
+        <p>React is so cool!</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library
+          for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a 
+          community of individual developers and companies. React can be used as a base in the development 
+          of single-page, mobile, or server-rendered applications with frameworks like Next.js. However, React is only 
+          concerned with state management and rendering that state to the DOM, so creating React applications usually requires 
+          the use of additional libraries for routing, as well as certain client-side functionality.
+          &nbsp;
+          <span>Source:&nbsp;
+            <a 
+              className="App-link" 
+              href="https://en.wikipedia.org/wiki/React_(JavaScript_library)"
+              target="_blank"
+              rel="author"
+              >
+                Wiki
+              </a>
+          </span>
         </p>
-        <p>Let's see what happens Ooooooo</p>      
+        <h2>React Frameworks:</h2>
+        <ul>
+          <li>Next.js</li>
+          <li>React Redux</li>
+          <li>Chakra UI</li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
